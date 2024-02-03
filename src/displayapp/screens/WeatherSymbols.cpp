44d3f524fdd1e18688f95eaf899025053132c5cf
JustScott <development@justscott.me>
2024-02-03 12:53:45 -0600

    Terminal Watchface Improvements and added Weather.
    Use @vkareh's new weather forecast app as a template to implement
    the weather into the terminal watch face. Use InfintimeTheme Colors
    instead of hardcoded hex values. Reorder code to match the order of
    the UI. Implement @vkareh's variable battery icon color to the battery
    percentage text. Added a new IninftimeTheme color, gray, and turn
    certain values gray when they contain no data, like weather.

--- src/displayapp/screens/WeatherSymbols.cpp
+++ src/displayapp/screens/WeatherSymbols.cpp
@@ -34,3 +34,28 @@ const char* Pinetime::Applications::Screens::Symbols::GetSymbol(const Pinetime::
       break;
   }
 }
+
+const char* Pinetime::Applications::Screens::Symbols::GetSimpleCondition(const Pinetime::Controllers::SimpleWeatherService::Icons icon) {
+  switch (icon) {
+    case Pinetime::Controllers::SimpleWeatherService::Icons::Sun:
+      return "Clear";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::CloudsSun:
+      return "Cloudy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::Clouds:
+      return "Cloudy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::BrokenClouds:
+      return "Cloudy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::CloudShowerHeavy:
+      return "Rainy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::CloudSunRain:
+      return "Rainy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::Thunderstorm:
+      return "Stormy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::Snow:
+      return "Snowy";
+    case Pinetime::Controllers::SimpleWeatherService::Icons::Smog:
+     return "Misty";
+    default:
+      return "";
+  }
+}
